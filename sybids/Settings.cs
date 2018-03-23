48078a5cd608032c652ea18c7129e3fa63716e87
ericcbohn <ericcbohn@gmail.com>
2018-03-23 10:21:13 -0500

    updated to local mongo instance

--- sybids/Settings.cs
+++ sybids/Settings.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace sybids {
     public class Settings {
-        public string ConnectionString;
+        public string ConnectionString;  //"ConnectionString": "mongodb://sybids:Million1@sybids-shard-00-00-o8hfg.mongodb.net:27017,sybids-shard-00-01-o8hfg.mongodb.net:27017,sybids-shard-00-02-o8hfg.mongodb.net:27017/test?ssl=true&replicaSet=sybids-shard-0&authSource=admin",
         public string Database;
     }
 }
